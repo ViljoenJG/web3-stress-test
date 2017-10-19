6a1417a4308c72bb3b507779094f7a1f310348e1
Kobus Viljoen <kobusv@spsi.co.za>
2017-10-19 15:55:50 +0200

    using random strings as keys and values

--- index.js
+++ index.js
@@ -1,45 +1,48 @@
-let Web3 = require('web3');
-// let contract_address='0x9F586b879ef822045708f500CA209e0445Fec958';
-let contract_address='0x78763a94bD3f71436053d8dA9C8b87aCD03816ce';
-let contract_abi = [{"constant":true,"inputs":[],"name":"getTheIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"key","type":"bytes8"}],"name":"get","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes8"},{"name":"value","type":"bytes32"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"key","type":"bytes8"},{"indexed":false,"name":"value","type":"bytes32"}],"name":"SetEvent","type":"event"}];
+const Web3 = require('web3');
+const randomstring = require('randomstring');
+
 let web3;
 
 if (typeof web3 !== 'undefined') {
     web3 = new Web3(web3.currentProvider);
 } else {
-    // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
+let contract_address='0x78763a94bD3f71436053d8dA9C8b87aCD03816ce'; // address of deployed contract
+let contract_abi = [{"constant":true,"inputs":[],"name":"getTheIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"kill","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"key","type":"bytes8"}],"name":"get","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getIndex","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"key","type":"bytes8"},{"name":"value","type":"bytes32"}],"name":"set","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"key","type":"bytes8"},{"indexed":false,"name":"value","type":"bytes32"}],"name":"SetEvent","type":"event"}]; // contract interface
 let contract_instance = web3.eth.contract(contract_abi).at(contract_address);
 
-web3.personal.unlockAccount(web3.eth.accounts[0], '1qazxsw2');
+web3.personal.unlockAccount(web3.eth.accounts[0], '1qazxsw2'); // set account password
 
-for (let a=0; a<=10; a++) {
-    contract_instance.set(`key${ a }`, `Value of ${ a }`, { from: web3.eth.accounts[0] }, function (err, txHash) {
+for (let a=0; a<10; a++) {
+    let key = randomstring.generate(5);
+    let value = randomstring.generate(20);
+
+    contract_instance.set(key, value, { from: web3.eth.accounts[0] }, function (err, txHash) {
         if (err) {
-            console.error(err);
-        } else {
-            console.log(txHash);
-            callWhenMined(txHash, function () {
-                console.log(`mined: ${ txHash }`)
-            })
+            return console.error(err);
         }
-    })
+
+        console.log(txHash);
+        callWhenMined(txHash, function () {
+            console.log(`mined: ${ txHash }`);
+        });
+    });
 }
 
 function callWhenMined(txHash, callback) {
     web3.eth.getTransactionReceipt(txHash, function(error, rcpt) {
         if(error) {
-            console.error(error);
-        } else {
-            if(rcpt == null) {
-                setTimeout(function() {
-                    callWhenMined(txHash, callback);
-                }, 500);
-            } else {
-                callback();
-            }
+            return console.error(error);
         }
-    })
+
+        if(rcpt == null) {
+            setTimeout(function() {
+                callWhenMined(txHash, callback);
+            }, 500);
+        } else {
+            callback();
+        }
+    });
 }
